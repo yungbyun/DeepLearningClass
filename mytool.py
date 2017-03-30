3ab311258460e516c5b68fdd19ff51c2d5a7ef5d
bcart21 <byclink@gmail.com>
2017-03-30 10:46:13 +0900

    0333010

--- mytool.py
+++ mytool.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 #학습하는 동안 진행되는 것을 표시하기 위하여 점을 한줄로 찍도록 하는 코드. print('.')는 새로운 줄에 찍어버림.
 def write(str):
@@ -10,3 +11,5 @@ def load_mnist():
     # Check out https://www.tensorflow.org/get_started/mnist/beginners for more information about the mnist dataset
     return input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+def get_random_int(max):
+    return random.randint(0, max - 1)
