00dc48485c29111a46d89cd964e3649285532404
bcart21 <byclink@gmail.com>
2017-04-12 23:38:43 +0900

    041223

--- mytool.py
+++ mytool.py
@@ -43,5 +43,26 @@ x_data = np.array([[h]], dtype=np.float32) # x_data = [[[1,0,0,0]]] #rank 3,
 pp = pprint.PrettyPrinter(indent=4)
 
 
+x_data = np.arange(45, dtype=np.float32).reshape(batch_size, sequence_length, input_dim)
+print(x_data)
+
+[[[  0.   1.   2.]
+  [  3.   4.   5.]
+  [  6.   7.   8.]
+  [  9.  10.  11.]
+  [ 12.  13.  14.]]
+
+ [[ 15.  16.  17.]
+  [ 18.  19.  20.]
+  [ 21.  22.  23.]
+  [ 24.  25.  26.]
+  [ 27.  28.  29.]]
+
+ [[ 30.  31.  32.]
+  [ 33.  34.  35.]
+  [ 36.  37.  38.]
+  [ 39.  40.  41.]
+  [ 42.  43.  44.]]]
+
 '''
 
