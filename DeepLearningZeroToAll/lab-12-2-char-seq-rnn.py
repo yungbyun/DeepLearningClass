ea74268662ef34f875f93b8c7f217b1b02badf8e
iclab <iclab@iclab-ui-Mac-Pro.local>
2017-04-15 23:25:51 +0900

    041523

--- DeepLearningZeroToAll/lab-12-2-char-seq-rnn.py
+++ DeepLearningZeroToAll/lab-12-2-char-seq-rnn.py
@@ -1,51 +1,62 @@
 # Lab 12 Character Sequence RNN
 import tensorflow as tf
 import numpy as np
-tf.set_random_seed(777)  # reproducibility
 
-sample = " if you want you"
-idx2char = list(set(sample))  # index -> char
-char2idx = {c: i for i, c in enumerate(idx2char)}  # char -> idex
 
-# hyper parameters
-dic_size = len(char2idx)  # RNN input size (one hot size)
-rnn_hidden_size = len(char2idx)  # RNN output size
-num_classes = len(char2idx)  # final output size (RNN or softmax, etc.)
-batch_size = 1  # one sample data, one batch
-sequence_length = len(sample) - 1  # number of lstm rollings (unit #)
+class XXX:
+    def run(self):
+        tf.set_random_seed(777)  # reproducibility
 
-sample_idx = [char2idx[c] for c in sample]  # char to index
-x_data = [sample_idx[:-1]]  # X data sample (0 ~ n-1) hello: hell
-y_data = [sample_idx[1:]]   # Y label sample (1 ~ n) hello: ello
+        sample = " if you want you"
 
-X = tf.placeholder(tf.int32, [None, sequence_length])  # X data
-Y = tf.placeholder(tf.int32, [None, sequence_length])  # Y label
+        idx2char = list(set(sample))  # index -> char
 
-x_one_hot = tf.one_hot(X, num_classes)  # one hot: 1 -> 0 1 0 0 0 0 0 0 0 0
-cell = tf.contrib.rnn.BasicLSTMCell(
-    num_units=rnn_hidden_size, state_is_tuple=True)
-initial_state = cell.zero_state(batch_size, tf.float32)
-outputs, _states = tf.nn.dynamic_rnn(
-    cell, x_one_hot, initial_state=initial_state, dtype=tf.float32)
+        char2idx = {c: i for i, c in enumerate(idx2char)}  # char -> idex
 
-weights = tf.ones([batch_size, sequence_length])
-sequence_loss = tf.contrib.seq2seq.sequence_loss(
-    logits=outputs, targets=Y, weights=weights)
-loss = tf.reduce_mean(sequence_loss)
-train = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss)
+        # hyper parameters
+        dic_size = len(char2idx)  # RNN input size (one hot size)
+        rnn_hidden_size = len(char2idx)  # RNN output size
+        num_classes = len(char2idx)  # final output size (RNN or softmax, etc.)
+        batch_size = 1  # one sample data, one batch
+        sequence_length = len(sample) - 1  # number of lstm rollings (unit #)
 
-prediction = tf.argmax(outputs, axis=2)
+        sample_idx = [char2idx[c] for c in sample]  # char to index
+        x_data = [sample_idx[:-1]]  # X data sample (0 ~ n-1) hello: hell
+        y_data = [sample_idx[1:]]   # Y label sample (1 ~ n) hello: ello
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    for i in range(3000):
-        l, _ = sess.run([loss, train], feed_dict={X: x_data, Y: y_data})
-        result = sess.run(prediction, feed_dict={X: x_data})
+        X = tf.placeholder(tf.int32, [None, sequence_length])  # X data
+        Y = tf.placeholder(tf.int32, [None, sequence_length])  # Y label
 
-        # print char using dic
-        result_str = [idx2char[c] for c in np.squeeze(result)]
+        x_one_hot = tf.one_hot(X, num_classes)  # one hot: 1 -> 0 1 0 0 0 0 0 0 0 0
+        cell = tf.contrib.rnn.BasicLSTMCell(
+            num_units=rnn_hidden_size, state_is_tuple=True)
+        initial_state = cell.zero_state(batch_size, tf.float32)
+        outputs, _states = tf.nn.dynamic_rnn(
+            cell, x_one_hot, initial_state=initial_state, dtype=tf.float32)
 
-        print(i, "loss:", l, "Prediction:", ''.join(result_str))
+        weights = tf.ones([batch_size, sequence_length])
+        sequence_loss = tf.contrib.seq2seq.sequence_loss(
+            logits=outputs, targets=Y, weights=weights)
+        loss = tf.reduce_mean(sequence_loss)
+        train = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss)
+
+        prediction = tf.argmax(outputs, axis=2)
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for i in range(100): #3000
+                l, _ = sess.run([loss, train], feed_dict={X: x_data, Y: y_data})
+                result = sess.run(prediction, feed_dict={X: x_data})
+
+                # print char using dic
+                result_str = [idx2char[c] for c in np.squeeze(result)]
+
+                print(i, "loss:", l, "Prediction:", ''.join(result_str))
+
+
+gildong = XXX()
+gildong.run()
+
 
 
 '''
